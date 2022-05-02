37d0cdcda9aab97371090421daea570238927409
hanseu9839 <hanseu9839@gmail.com>
2022-05-02 21:35:27 +0900

    habit props

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -15,11 +15,11 @@ class Habit extends Component {
     };
 
     render() {
-        console.log(this.props);
+        const { name , count } = this.props.habit;
         return (
         <li className="habit">
-            <span className="habit-name">Reading</span>
-            <span className="habit-count">{this.state.count}</span>
+            <span className="habit-name">{name}</span>
+            <span className="habit-count">{count}</span>
             <button className="habit-button habit-increase" onClick={this.handleIncrement}>
                 <i className="fa-solid fa-circle-plus"></i>
             </button>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -4,9 +4,9 @@ import Habit from './habit';
 class Habits extends Component {
     state = {
         habits:[
-            {name:'Reading',count:0},
-            {name:'Running',count:0},
-            {name:'Coding',count:0}
+            {id: 1, name:'Reading',count:0},
+            {id: 2, name:'Running',count:0},
+            {id: 3,name:'Coding',count:0}
         ],
     };
     render() {
@@ -14,7 +14,7 @@ class Habits extends Component {
         <ul>
             {
                 this.state.habits.map(habit => (
-                    <Habit habit={habit}/>
+                    <Habit key = {habit.id} habit={habit}/>
                 ))
             }
         </ul>
